534afd88591da3e1d14d2a216fa27c655ca502a7
k0deek <d.kotenko@g.nsu.ru>
2022-05-12 21:37:52 +0700

    WebShtorm№1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './style/App.css';
+import './style/userpage.css';
+import './style/listpage.css';
+import HeadMain from "./components/Head"
+import logo from './resources/img/logo.png';
+import user from './resources/img/user.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <HeadMain/>
+
+        </div>
+    );
 }
 
 export default App;
+
+
+
+
